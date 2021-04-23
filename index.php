725677464c646b4a5c389b5839860d5cc37d577a
TalesCostta <talessilvacostta@gmail.com>
2021-04-23 20:50:27 -0300

    Novo arquivo index, para executar junto com o Wordpress

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css"  href="style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="<?php echo get_theme_root_uri(); ?> /SiteDenizeB/style.css" rel="stylesheet">
+    <title>Blog da Denize</title>
+</head>
+<body>
+    <header>
+        <ul class="menu">
+           <li><a href="#">
+                <img src="<?php echo get_theme_root_uri(); ?> /SiteDenizeB/images/IMG_5395.PNG">
+                <div class="denize">
+                    <h2>Denize</h2>
+                </div>
+                <div class="olerante">
+                    <h2>Olerante</h2>
+                </div>
+            </a>
+            </li>
+            <li><a class="conteudo" href="#"><h2>Conteúdo Blog</h2></a></li>
+            <li><a class="contato" href="#"><h2>Contato</h2></a></li>
+            <div class="linha-vertical"></div>
+            <li><a class="insta" href="#"><h2>denizeolerante</h2></a></li>
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+        </ul>
+        <section class="linha-horizontal"></section>
+    </header>
+    <section class="fundo-verde"></section>
+    <section class="capa"><img src="<?php echo get_theme_root_uri(); ?> /SiteDenizeB/images/Capa Denize.jpeg"></section>
+    <section class="linha-capa"></section>
+    <section class="pag2">
+        <img src="<?php echo get_theme_root_uri(); ?> /SiteDenizeB/images/Foto Denize.jpeg">
+        <section class="linha-sobre"></section>
+        <h2>Denize Olerante</h2>
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
+    </section>
+    <section class="aba-noticias">
+        <h2>Conteúdo Blog</h2>
+        <section class="linha-blog"></section>
+        <element class="separar-pagina"></element>
+        <element class="separar-pagina2"></element>
+    </section>
+    <section class="aba-contato">
+        <h2>Endereço:</h2>
+        <element class="linha-endereco"></element>
+        <h3 class="linha1">Av. B, 155, Quadra 08 - Lote 01 <h3 class="linha2">Itanhangá I</h3> <h3 class="linha3">CEP 75.690-000</h3><h3 class="linha4">Caldas Novas - Goiás</h3>
+    </section>
+    <section class="redes-sociais">
+        <element class="fundo"></element>
+        <h2 class="titulo">Redes Sociais</h2>
+        <div class="telegram">
+            <a href="#"><h3>+55 64 9 8421-0417</h3></a>
+            <i class="fa fa-telegram" aria-hidden="true"></i>
+        </div>
+        <div class="whatsapp">
+            <a href="#"><h3>+55 64 9 8421-0417</h3></a>
+            <i class="fa fa-whatsapp" aria-hidden="true"></i>
+        </div>
+        <div class="insta">
+            <a href="#"><h3>@denizeolerante</h3></a>
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+        </div>
+        <div class="facebook">
+            <a href="#"><h3>facebook.com/denize.olerante</h3></a>
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+        </div>
+        <div class="email">
+            <a href="#"><h3>denizeolerante@gmail.com</h3></a>
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+        </div>
+    </section>
+    <section class="rodape">
+        <element class="fundo"></element>
+        <h2 class="direitos"> Denize Olerante © 2021 - Todos direitos reservados.</h2>
+        <h2 class="dev">Desenvolvido por Tales Costa.</h2>
+    </section>
+    <section class="fundo-horizontal-grande"></section>
+</body>
+</html>
